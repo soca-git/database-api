c1ab7c045b190b185bfc224b5dbac65b7983823d
soca-git <soca.mailbox@gmail.com>
2022-12-14 13:33:56 +0000

    add ping endpoint

---
+++ src/Database.Controllers/Infrastructure/Ping/PingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Database.Controllers.Infrastructure.Ping
+{
+    /// <summary>
+    /// </summary>
+    [ApiController]
+    [Route("api/database/ping")]
+    public class PingController : ControllerBase
+    {
+        /// <summary>
+        /// Returns a friendly message if the server is accessible.
+        /// </summary>
+        public async Task<string> Get()
+        {
+            return await Task.FromResult("Hello from Database-API!");
+        }
+    }
+}
